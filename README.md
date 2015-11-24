7f2d2c80a6de3103e17eb639ff23ee9c1c879ef0
Soren <hkh222>
2015-11-24 17:04:17 +0100

    some minor changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # dbscan
 DBSCAN implementation in Python (based on Jaccard Distance)
 
-This code is written for an assignment in Big Data course. Use the test iPython notebook file to fiddle with the code, you may find some hidden gems! There are some test files in the data folder with different dimensions.
+This code is written for an assignment in Big Data course. It is only optimized for the given datasets in the data folder. Use the test iPython notebook file to fiddle with the code, you may find some hidden gems!
--- dbscan.py
+++ dbscan.py
@@ -1,111 +1,117 @@
-from time import gmtime, strftime, time
-from collections import Counter
+from time import time
+from collections import Counter, defaultdict
+
 
 def dbscan(mat, eps=.15, min_pts=2, report=False):
-    """
+    '''
     Density-based spatial clustering of applications with noise (DBSCAN).
 
     :param mat: scipy csr_matrix format
     :param eps: distance epsilon
     :param min_pts: minimum number of points required to form a dense region
     :param report: en/disable measuring duration of each process and reporting
-    :return: returns nothing
-    """
+    :return: returns clusters
+    '''
     # --------------------------------------------------------------------------
-    if report:
-        print "Converting to dictionary",
-        init_time = time()
 
-    # @dictionary:
+    if report:
+        print 'Converting to dictionary',
+        init_t = time()
+
+    # @condensed:
     # - keys: frozenset of the index of the nonzero entries of the matrix rows
-    # - values: set of the row indices
+    # - values: list of the original row indices
     # Note: frozenset is hashable and can be used as a key unlike regular python set.
-    # This is useful when obtaining the intersection in Jaccard Distance calculation.
-    dictionary = {}
+    # This is useful when obtaining the intersection in Jaccard Distance
+    # calculation.
+    condensed = defaultdict(list)
 
-    # Note: accessing raw data (indices and indptr attributes of csr_matrix) had 30x
-    # performance boost compared to normal indexing/slicing/iteration.
-    i = 0
-    for idx, j in enumerate(mat.indptr[1:]):
-        key = frozenset(mat.indices[i:j])
-        i = j
-        if key in dictionary:
-            dictionary[key].add(idx)
-        else:
-            dictionary[key] = set([idx])
-
-    # this is used later for optimizing construction of neighbors matrix
-    lengths = Counter()
-    keys = dictionary.keys()
-    for i, key in enumerate(keys):
-        lengths[i] = len(key)
+    # Note: accessing raw data (indices and indptr attributes of csr_matrix)
+    # had 30x performance boost compared to normal indexing/slicing/iteration
+    # of sparse matrices.
+    j = 0
+    for i, k in enumerate(mat.indptr[1:]):
+        condensed[frozenset(mat.indices[j:k])].append(i)
+        j = k
 
     if report:
-        print "is done in %.1fs." % (time() - init_time)
+        print 'is done in %d ms.' % ((time() - init_t) * 1000)
         # ----------------------------------------------------------------------
-        print "Constructing neighbors matrix",
+        print 'Constructing adjacency matrix',
         t = time()
 
-    # initializing neighbors
-    neighbors = [[] for i in range(len(dictionary))]
+    # initializing the adjacency matrix
+    adjacency = [[] for i in range(len(condensed))]
 
-    # constructing neighbors matrix
-    sorted_lengths = lengths.most_common()
-    for idx, (i, i_len) in enumerate(sorted_lengths):
-        # lengths[idx+1:]: iterating over half-diagonal, and also excluding the
+    keys = condensed.keys()
+    # sorting the points based on number of elements they contains
+    sorted_lens = Counter({i: len(key) for i, key in enumerate(keys)}).most_common()
+
+    # constructing adjacency matrix
+    for k, (i, i_len) in enumerate(sorted_lens):
+        # lengths[k + 1:]: iterating over half-diagonal, and also excluding the
         # diagonal itself since they are alway true
-        for i_neibor, i_neibor_len in sorted_lengths[idx + 1:]:
+        for j, j_len in sorted_lens[k + 1:]:
             # initial check assumming the best case scenario in which one of
             # the points is subset of the other one. If this neighbor doesn't
             # pass the test, the rest won't either because lengths are sorted
             # so we BREAK the loop. This had 3x performance boost compared to
             # unsorted length case.
-            if i_neibor_len < (1 - eps) * i_len:
+            if j_len < (1 - eps) * i_len:
                 break
 
             # calculating proximity based on Jaccard distance
-            intersection = len(keys[i] & keys[i_neibor])
+            intersection = len(keys[i] & keys[j])
+
             # from math: a U b = a + b - a I b
-            if 1 - 1.0 * intersection / (i_len + i_neibor_len - intersection) <= eps:
-                neighbors[i].append(i_neibor)
-                neighbors[i_neibor].append(i)
+            if 1 - 1.0 * intersection / (i_len + j_len - intersection) <= eps:
+                adjacency[i].append(j)
+                adjacency[j].append(i)
 
     if report:
-        print "is done in %.1fs." % (time() - t)
+        print 'is done in %d ms.' % ((time() - t) * 1000)
         # ----------------------------------------------------------------------
-        print "DBSCANning",
+        print 'DBSCANning',
         t = time()
 
-    # constructing noise cluster
-    visited_noise = [False] * len(dictionary)
-    noise = set([])
-    for i, i_neighbors in enumerate(neighbors):
-        if len(i_neighbors) + len(dictionary[keys[i]]) < min_pts:
-            # Restoring original indices of the nodes
-            noise.update(dictionary[keys[i]])
-            visited_noise[i] = True
+    # constructing noise cluster and marking them as visited
+    visited = [False] * len(condensed)
+    clusters = [[]]
+    for i, neighbors in enumerate(adjacency):
+        if len(neighbors) + len(condensed[keys[i]]) < min_pts:
+            clusters[0].append(i)
+            visited[i] = True
 
     # searching for clusters
-    clusters = []
-    for i, i_neighbors in enumerate(neighbors):
-        if not visited_noise[i]:
-            visited_noise[i] = True
-            # dictionary[keys[i]] restores the original indices of the nodes
-            clusters.append(dictionary[keys[i]])
-            for i_neibor in i_neighbors:
-                if not visited_noise[i_neibor]:
-                    visited_noise[i_neibor] = True
-                    for i_neibor_neibor in neighbors[i_neibor]:
-                        if not visited_noise[i_neibor_neibor]:
-                            i_neighbors.append(i_neibor_neibor)
+    for i, neighbors in enumerate(adjacency):
+        if not visited[i]:
+            visited[i] = True
+            clusters.append(set([i]))
+            for j in neighbors:
+                if not visited[j]:
+                    visited[j] = True
+                    neighbors.extend([k for k in adjacency[j] if not visited[k]])
+
                 # index -1 is the last item in the list
-                clusters[-1].update(dictionary[keys[i_neibor]])
+                clusters[-1].add(j)
 
     if report:
-        print "is done in %.1fs." % (time() - t)
-        print "Program finished after %.1fs.\n" % (time() - init_time)
+        print 'is done in %d ms.' % ((time() - t) * 1000)
+        # ----------------------------------------------------------------------
+        print 'Restoring original point indices',
+        t = time()
 
-        print "Largest cluster size is:\t", max(map(len, clusters))
-        print "Total number of clusters:\t", len(clusters) + 1
-        print "Number of members in noise:\t", len(noise)
-        # print "Number of members in each cluster:\t", map(len, clusters)
+    # restoring original indices that was lost when condensing the matrix in
+    # the beginning
+    clusters = [[i for j in c for i in condensed[keys[j]]] for c in clusters]
+
+    if report:
+        print 'is done in %d ms.' % ((time() - t) * 1000)
+        print '\nProgram finished after %d ms.\n' % ((time() - init_t) * 1000)
+
+        print 'Total number of clusters:', len(clusters)
+        print 'Largest cluster size is:', max(map(len, clusters[1:]))
+        print 'Noise size:', len(clusters[0])
+#         print 'Number of members in each cluster:', map(len, clusters[1:])
+
+    return clusters
